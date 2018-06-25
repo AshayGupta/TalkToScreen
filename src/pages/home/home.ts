9ca0b5e63ef0557deaa840eaeebdab36128c270b
Ashay Gupta <ashaygupta10@gmail.com>
2018-06-26 00:26:00 +0530

    speech recognition methods

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,46 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private speechRecognition: SpeechRecognition) {
+    this.isSpeechRecognitionAvailable()
   }
 
+  private isSpeechRecognitionAvailable() {
+    this.speechRecognition.isRecognitionAvailable().then((available: boolean) => {
+      console.log(available)
+      if (available) {
+        this.hasPermission()
+      }
+    })
+  }
+
+  private hasPermission() {
+    this.speechRecognition.hasPermission().then((hasPermission: boolean) => {
+      console.log(hasPermission)
+      if (hasPermission) {
+        this.startListening()
+      }
+      else {
+        this.requestPermission()
+      }
+    })
+  }
+
+  private requestPermission() {
+    this.speechRecognition.requestPermission().then(() => {
+      console.log('Granted')
+    },() => {
+      console.log('Denied')
+    })
+  }
+
+  private startListening() {
+    this.speechRecognition.startListening().subscribe((matches: Array<string>) => {
+      console.log(matches)
+    }, (onerror) => {
+      console.log('error:', onerror)
+    })
+  }
 }
+
+
